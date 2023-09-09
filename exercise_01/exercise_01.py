de15355d7b39b54e688be72b59afeb253860a2f1
Jared Hawkins-Young <jahy81802@gmail.com>
2023-09-09 12:04:58 -0400

    bank account publishing

--- exercise_01/exercise_01.py
+++
@@ -1,1 +0,0 @@
-print("Bank Account")
---
+++ exercise_1/main.py
@@ -0,0 +1,6 @@
+from exercise_01 import BankAccount
+if __name__ == "__main__":
+    account = BankAccount("Jared", 1000000)
+    account.deposit(350000)
+    account.withdraw(500000)
+    print(account.get_balance())
